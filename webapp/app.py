5a05b47bfa5e0d7288ad07223703576a67371e79
shivanshuasthana81 <shivanshuasthana446@gmail.com>
2026-04-14 12:56:57 +0530

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ import requests
 from werkzeug.utils import secure_filename
 
 from flask import Flask, render_template, redirect, url_for, request, flash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
